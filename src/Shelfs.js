5c265410629a6edd7e7ea70d45549f464e022fe6
Bruno Gurgel <bmg1612@gmail.com>
2021-02-11 15:29:49 -0300

    feat: Used the books object sent through props to render the UI

--- src/Shelfs.js
+++ src/Shelfs.js
@@ -1,19 +1,23 @@
 import React from "react";
 import Book from "./Book";
+//import * as BooksAPI from "./BooksAPI";
 
-function Shelfs() {
+function Shelfs(props) {
   return (
     <div>
       <div className="bookshelf">
         <h2 className="bookshelf-title">Currently Reading</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
-            <li>
-              <Book />
-            </li>
+            {props.booksObject.currentlyReading.map((book) => (
+              <li key={book.id}>
+                <Book
+                  backgroundURL={book.imageLinks.thumbnail}
+                  bookTitle={book.title}
+                  bookAuthor={book.authors}
+                />
+              </li>
+            ))}
           </ol>
         </div>
       </div>
@@ -21,12 +25,15 @@ function Shelfs() {
         <h2 className="bookshelf-title">Want to Read</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
-            <li>
-              <Book />
-            </li>
+            {props.booksObject.wantToRead.map((book) => (
+              <li key={book.id}>
+                <Book
+                  backgroundURL={book.imageLinks.thumbnail}
+                  bookTitle={book.title}
+                  bookAuthor={book.authors}
+                />
+              </li>
+            ))}
           </ol>
         </div>
       </div>
@@ -34,15 +41,15 @@ function Shelfs() {
         <h2 className="bookshelf-title">Read</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
-            <li>
-              <Book />
-            </li>
-            <li>
-              <Book />
-            </li>
+            {props.booksObject.read.map((book) => (
+              <li key={book.id}>
+                <Book
+                  backgroundURL={book.imageLinks.thumbnail}
+                  bookTitle={book.title}
+                  bookAuthor={book.authors}
+                />
+              </li>
+            ))}
           </ol>
         </div>
       </div>
