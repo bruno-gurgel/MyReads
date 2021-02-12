5973a550820f9f0ccf12a03aa2e68ef8883401ed
Bruno Gurgel <bmg1612@gmail.com>
2021-02-12 17:27:33 -0300

    feat: Added react routers Link

--- src/Search.js
+++ src/Search.js
@@ -1,24 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import Book from "./Book";
 
 function Search(props) {
   return (
     <div className="search-books">
       <div className="search-books-bar">
-        <button
-          className="close-search"
-          onClick={() => props.handleSearchPage(false)}
-          // onClick={() => this.setState({ showSearchPage: false })}
-        >
+        <Link to="/" className="close-search">
           Close
-        </button>
+        </Link>
         <div className="search-books-input-wrapper">
-          {/*
-          NOTES: The search from BooksAPI is limited to a particular set of search terms.
-          You can find these search terms here:
-          https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-          However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-          you don't find a specific author or title. Every search is limited by search terms.
-        */}
           <input type="text" placeholder="Search by title or author" />
         </div>
       </div>
@@ -28,4 +20,9 @@ function Search(props) {
     </div>
   );
 }
+
+Search.propTypes = {
+  booksArray: PropTypes.array.isRequired,
+};
+
 export default Search;
