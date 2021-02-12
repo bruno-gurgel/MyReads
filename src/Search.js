bfeb4e98528926b790f440b24a6057d155c9128a
Bruno Gurgel <bmg1612@gmail.com>
2021-02-12 17:29:12 -0300

    feat: Added search functionality

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Book from "./Book";
+import * as BooksAPI from "./BooksAPI";
 
 function Search(props) {
+  const [query, updateQuery] = useState("");
+  const [booksResults, updateBooksResults] = useState([]);
+
+  const handleQuery = (event) => {
+    updateQuery(event.target.value);
+
+    try {
+      BooksAPI.search(query).then((searchResults) =>
+        updateBooksResults(searchResults)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="search-books">
       <div className="search-books-bar">
@@ -11,11 +27,40 @@ function Search(props) {
           Close
         </Link>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author" />
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            value={query}
+            onChange={(event) => handleQuery(event)}
+          />
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {query && booksResults && !booksResults.error ? (
+            booksResults.map((book) => (
+              <li key={book.id}>
+                <Book
+                  backgroundURL={
+                    book.imageLinks ? book.imageLinks.thumbnail : ""
+                  }
+                  bookTitle={book.title}
+                  bookAuthor={book.authors}
+                  shelf={book.shelf}
+                  book={book}
+                  updateShelf={props.updateShelf}
+                />
+              </li>
+            ))
+          ) : query && booksResults ? (
+            <p className="error-text">
+              There are no results for your query:
+              <span className="error-span">"{query}"</span>
+            </p>
+          ) : (
+            ""
+          )}
+        </ol>
       </div>
     </div>
   );
